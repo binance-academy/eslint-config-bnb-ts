2bc9f7f207a51cd9b5b56f93c1dd3010af5c9287
Eric Yip <letitbemine@gmail.com>
2021-03-22 16:54:54 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# eslint-config-bnb
+
+This package provide Binance Academy's eslint config. It uses [Airbnb's eslint config](https://www.npmjs.com/package/eslint-config-airbnb) and [Prettier config](https://github.com/prettier/eslint-config-prettier)
+
+## Usage
+
+```
+npx install-peerdeps --dev @binance-academy/eslint-config-bnb
+```
+
+Add `"extends": "@binance-academy/eslint-config-bnb"` to your eslint config file.
---
+++ index.js
@@ -0,0 +1,43 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2017,
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    es6: true,
+    browser: true,
+  },
+  plugins: ['@typescript-eslint'],
+  extends: ["airbnb", "airbnb/hooks", "prettier"],
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx", ".d.ts"],
+    },
+    "import/resolver": {
+      "node": {
+        "extensions": [".js", ".jsx", ".ts", ".tsx", ".json", ".d.ts"],
+      }
+    }
+  },
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "import/extensions": "off",
+    "react/forbid-prop-types": [1, { forbid: ["any"] }],
+    "react/prop-types": ["error", { ignore: ["children"] }],
+    "react/jsx-filename-extension": 0,
+    "react/no-multi-comp": 0,
+    "react/jsx-props-no-spreading": 0,
+  },
+  overrides: [
+    {
+      files: ['**/*.tsx'],
+      rules: {
+        'react/prop-types': 'off',
+      },
+    },
+  ]
+};
